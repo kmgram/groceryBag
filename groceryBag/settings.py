f503dde3067f38672f92fd5282df250438c27029
kmgram <ramachandra.km@gmail.com>
2021-08-16 15:39:41 +0530

    Update settings.py

--- groceryBag/settings.py
+++ groceryBag/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ub!!m_sms24vdgj0&z^x6oaopi5f+bezdgm2*y_0c%v$wlw5+)'
+SECRET_KEY = []
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
