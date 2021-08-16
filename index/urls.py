4c5996d83f9251fe71406210e35199fb41189575
kmgram <ramachandra.km@gmail.com>
2021-08-16 23:56:35 +0530

    'deleted extra templates'

--- index/urls.py
+++ index/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name = 'home-view'),
+    path('', views.dashboard, name = 'dashboard-view'),
     path('register/', views.registration_page, name = 'registration-view'),
     path('login/', views.login_page, name = 'login-view'),
     path('logout/', views.logout_user, name = 'logout-view'),
-    path('dashboard/', views.query_grocery, name = 'dashboard-view'),
+    #path('dashboard/', views.query_grocery, name = 'dashboard-view'),
     path('additem/', views.add_item, name = 'add-view'),
     path('updateitem/<input_arg>/', views.update_item, name = 'update-view'),
     path('dashboard/<input_arg>/', views.add_update, name = 'add_update-view'),
--- index/views.py
+++ index/views.py
@@ -5,20 +5,12 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from . models import GroceryList
-import time
 
-# Create your views here.
-@login_required(login_url = '/login')
-def home(request):
-     
-    return render(request,'index/home.html')
 
 def registration_page(request):
     if request.user.is_authenticated:
-        return redirect('home-view')
-    
-    else:   
-
+        return redirect('dashboard-view')
+    else:  
         if request.method == "POST":
             form = CustomUserCreation(request.POST)
             if form.is_valid():
@@ -32,78 +24,70 @@ def registration_page(request):
 
 def login_page(request):
     if request.user.is_authenticated:
-        return redirect('home-view')
+        return redirect('dashboard-view')
     else:
         if request.method == "POST":
             username = request.POST.get('username')
             password = request.POST.get('password')
             user = authenticate(request,username=username,password=password)
             if user is not None:
-                login(request,user)
-                
-                return redirect('home-view')
+                login(request,user)                
+                return redirect('dashboard-view')
             else:
                 messages.info(request, 'Login attempt failed; username or password incorrect')
-
         return render(request,'index/login.html')
 
 def logout_user(request):
     logout(request)
     return redirect('login-view')
 
-def query_grocery(request):
-    if request.user.is_authenticated:
-        current_user = request.user
-        print(current_user)
-    if request.method == 'POST':
-        print('query_grocery requested method:', request.method)
+@login_required(login_url = '/login')
+def dashboard(request):
+    if request.method == 'POST':        
         date_to_filter = request.POST.get('date_select')
-        print('dateToSelect:',date_to_filter)
-        querydata = GroceryList.objects.filter(date_created=date_to_filter,user=request.user)
-        #usrname = request.get_username()
-        return render(request,'index/dashboard.html', {'querydata':querydata})
-    else:
-        print('query_grocery requested method:', request.method)
+        if date_to_filter:                
+            querydata = GroceryList.objects.filter(date_created=date_to_filter,user=request.user)       
+            return render(request,'index/dashboard.html', {'querydata':querydata})
+        else:
+            messages.warning(request,'Select the date before applying filter')
+            querydata = GroceryList.objects.filter(user=request.user)
+            return render(request,'index/dashboard.html', {'querydata':querydata})
+       
+    else:        
         querydata = GroceryList.objects.filter(user=request.user)
-        return render(request,'index/dashboard.html', {'querydata':querydata})
-
-
+        return render(request,'index/dashboard.html', {'querydata':querydata}) 
+   
     
-    
-    
-
+@login_required(login_url = '/login')
 def add_item(request):
     if request.method == 'GET':
         form_grocery = ModelFormGroceryList()
         return render(request, 'index/add.html',{'form_grocery': form_grocery})
-    elif request.method == 'POST':
-        if request.user.is_authenticated:
-            current_user = request.user        
-            data_mdfy = ModelFormGroceryList(request.POST)
-            #data_mdfy.user = current_user
-            #print(data_mdfy.user)
-            if data_mdfy.is_valid():
-                print('data is valid')          
-            
-                item_name_request = (request.POST.get('name'))
-                item_name_db = GroceryList.objects.filter(name=item_name_request,user=request.user)         
-                if GroceryList.objects.filter(name__iexact=item_name_request,user=request.user):
-                    messages.warning(request, f'{item_name_request} is already present in the list,\
-                     use "Update" button on dashboard to modify the list')
-                    return redirect('add-view')
-                else:
-                    print('before save')
-                    obj=data_mdfy.save(commit=False)
-                    obj.user=request.user
-                    obj.save()
-               
-                    messages.success(request, 'Your request to add item is successful')
-                    return redirect('dashboard-view')
+    elif request.method == 'POST':        
+        data_mdfy = ModelFormGroceryList(request.POST)
+        if data_mdfy.is_valid():                  
+            item_name_request = (request.POST.get('name'))
+            item_name_db = GroceryList.objects.filter(name=item_name_request,user=request.user)         
+            if GroceryList.objects.filter(name__iexact=item_name_request,user=request.user):
+                messages.error(request, f'{item_name_request} is already present in the list,\
+                    use "Update" button on dashboard to modify the list')
+                return redirect('add-view')
+            else:                
+                obj=data_mdfy.save(commit=False)
+                obj.user=request.user
+                obj.save()            
+                messages.success(request, 'Your request to add item is successful')
+                return redirect('dashboard-view')
         else:
+            form_grocery = ModelFormGroceryList(request.POST)
+            messages.error(request, 'Data validation failed, input correct details')
+            return render(request, 'index/add.html',{'form_grocery': form_grocery})
             
-            messages.error(request, 'Validation failed, Quntity cannot be negative value')
-            return redirect('add-view')
+            
 
+
+
+@login_required(login_url = '/login')
 def update_item(request,input_arg=''):
     if request.method == 'GET':
         if input_arg:
@@ -113,22 +97,22 @@ def update_item(request,input_arg=''):
     elif request.method == 'POST':
         obj_db = GroceryList.objects.get(id=input_arg)
         form_instance = ModelFormGroceryList(request.POST,instance=obj_db)
-        form_instance.user = request.user
-       
-        if form_instance.is_valid():
-            #obj=form_instance.save(commit=False)
-            #obj.user = request.user
-            print('Currrrent user', form_instance.user)
+        form_instance.user = request.user       
+        if form_instance.is_valid():                                  
             form_instance.save()
             messages.success(request, 'Your request to update item is successful')
             return redirect('dashboard-view')
+        else:
+            
+            messages.error(request, 'Data validation failed, input correct details')
+            return render(request, 'index/update.html',{'form_instance': form_instance})
 
 
+@login_required(login_url = '/login')
 def delete_item(request,input_arg=''):
     if request.method == 'GET':
         if input_arg:
-            obj_db = GroceryList.objects.get(id=input_arg)
-            #form_instance = ModelFormGroceryList(instance=obj_db)
+            obj_db = GroceryList.objects.get(id=input_arg)            
             return render(request,'index/delete.html', {'form_instance': obj_db})
     if request.method == 'POST':
         delete_db_obj = GroceryList.objects.get(id=input_arg)        
@@ -140,15 +124,6 @@ def delete_item(request,input_arg=''):
 
 
 def add_update(request,input_arg=''):
-    # if input_arg == 'date_filter':
-    #     print('date filter requested method:', request.method)
-    #     print('date select:', request.GET('date_select'))
-    #     date_to_filter = request.POST.get('date_select')
-    #     querydata = GroceryList.objects.all()
-    #     #querydata = GroceryList.objects.get(date_to_filter)
-    #     #usrname = request.get_username()
-    #     return render(request,'index/dashboard.html', {'querydata':querydata})
-
     if request.method == 'GET':
         if input_arg == 'add':
             return render(request,'index/add.html')
